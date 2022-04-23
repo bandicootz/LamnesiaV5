ff1e2bfaec5953f2435ad210a16a102cce229993
WhiteDemonRevan97 <Hooch141097@gmail.com>
2022-04-23 12:25:25 +0200

    New prototyp

--- Lamnesia  (Bartosz Wypych)/Assets/Scripts C#/ShootEnemy/SceneController.cs
+++ Lamnesia  (Bartosz Wypych)/Assets/Scripts C#/ShootEnemy/SceneController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject enemyyPrefab;
 
     private GameObject _enemy;
 
@@ -11,7 +11,7 @@ public class SceneController : MonoBehaviour
     {
         if (_enemy == null)
         {
-            _enemy = Instantiate(enemyPrefab) as GameObject;
+            _enemy = Instantiate(enemyyPrefab) as GameObject;
             _enemy.transform.position = new Vector3(0, 0, 0);
             float angle = Random.Range(0, 360);
             _enemy.transform.Rotate(0, angle, 0);
